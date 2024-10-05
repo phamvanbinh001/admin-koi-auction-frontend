6ca72800cb3ed5044c1a7f9a0d8e55db04f640ca
Pham Van Binh <binhpvqe180196@fpt.edu.vn>
2024-10-05 21:19:48 +0700

    update sidebar and header with awesome icon

--- src/components/Layout/DefaultLayout/Header/index.jsx
+++ src/components/Layout/DefaultLayout/Header/index.jsx
@@ -1,32 +1,33 @@
 // components/Header/index.jsx
-import { Layout, Avatar, Menu } from 'antd';
+import { Layout, Avatar, Input, Space, Badge } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Import FontAwesome icon
 
 const { Header } = Layout;
-
-const items = [
-  {
-    key: '1',
-    label: <Link to="/">Home</Link>,
-  },
-  {
-    key: '2',
-    label: <Link to="/about">About</Link>,
-  },
-];
+const { Search } = Input;
 
 const HeaderComponent = () => {
   return (
     <Header className="header" style={{ position: 'fixed', zIndex: 1, width: '100%', height: 64 }}>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['1']}
-        items={items} // Sử dụng items thay vì children
-        style={{ float: 'right', lineHeight: '64px' }}
-      />
+      <div className="logo">
+        <img
+          src="../../../../../public/images/logos.png"
+          alt="Koi-logo"
+          style={{ height: '20%', float: 'left', padding: '0 20px' }}
+        />
+      </div>
+
+      <Space direction="vertical" style={{ float: 'left', marginLeft: '20px' }}>
+        <Search placeholder="input search text" enterButton />
+      </Space>
+
+      {/* Badge for email notifications using FontAwesome icon */}
+      <Badge count={5} style={{ float: 'right', marginLeft: '16px', lineHeight: '64px' }}>
+        <FontAwesomeIcon icon={faEnvelope} size="lg" style={{ color: '#1677ff' }} />
+      </Badge>
+
+      {/* Avatar for user profile */}
       <Avatar icon={<UserOutlined />} style={{ float: 'right', marginLeft: '16px', lineHeight: '64px' }} />
     </Header>
   );
