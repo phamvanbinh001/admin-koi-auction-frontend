109e804c88ba4a7455d95cc203c52713929f7139
Pham Van Binh <pvbinh2k4@gmail.com>
2024-10-09 02:56:15 +0700

    fixing unsync

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
+import { AuthProvider } from './auth/AuthProvider'; // Đảm bảo rằng bạn đã import AuthProvider
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <AppRoutes />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </AuthProvider>
   );
-};
+}
 
 export default App;
