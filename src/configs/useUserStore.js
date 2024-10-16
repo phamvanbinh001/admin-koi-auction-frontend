d7f6e3b067d893d9485c9200a5a3a5468718d645
Pham Van Binh <pvbinh2k4@gmail.com>
2024-10-16 13:33:12 +0700

    Rebuilt layout part 3

--- src/configs/useUserStore.js
+++ src/configs/useUserStore.js
@@ -1,27 +1,73 @@
+// import { create } from 'zustand';
+
+// const unAuthenticatedUser = {
+//   id: null,
+//   role: '',
+//   username: '',
+//   fullname: '',
+//   isAuthenticated: false,
+//   token: null,
+// };
+
+// //Tạo store user
+// const useUserStore = create((set) => ({
+//   user: unAuthenticatedUser,
+
+//   loginUser: (userData) => {
+//     localStorage.setItem('user', JSON.stringify(userData));
+//     set({
+//       user: {
+//         ...userData,
+//         isAuthenticated: true,
+//       },
+//     });
+//   },
+
+//   logout: () => {
+//     localStorage.removeItem('user');
+//     set({
+//       user: unAuthenticatedUser,
+//     });
+//   },
+// }));
+
+// export default useUserStore;
+
 import { create } from 'zustand';
 
 const unAuthenticatedUser = {
   id: null,
-  fullName: '',
-  userName: '',
-  phoneNumber: '',
-  email: '',
-  address: null,
   role: '',
-  createAt: null,
-  updateAt: null,
-  status: '',
+  username: '',
+  fullname: '',
   isAuthenticated: false,
+  token: null,
 };
 
-//Tạo store user
+// Tạo store user
 const useUserStore = create((set) => ({
   user: unAuthenticatedUser,
 
   loginUser: (userData) => {
+    const { token, username, fullname, userId, role } = userData;
+
+    // Lưu thông tin người dùng vào localStorage
+    localStorage.setItem('user', JSON.stringify({
+      id: userId,
+      username,
+      fullname,
+      role,
+      token,
+      isAuthenticated: true,
+    }));
+
     set({
       user: {
-        ...userData,
+        id: userId,
+        username,
+        fullname,
+        role,
+        token,
         isAuthenticated: true,
       },
     });
@@ -36,3 +82,4 @@ const useUserStore = create((set) => ({
 }));
 
 export default useUserStore;
+
--- src/pages/401/index.jsx
+++ src/pages/401/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from './index.module.scss';
 
 const Page401 = () => {
-  return <img src="src\assets\401.png" alt="401 Page"></img>;
+  console.log('render Page401');
+
+  return (
+    <div className={styles.container}>
+      <img src="src/assets/401.png" alt="401 Page" className={styles.image} />
+    </div>
+  );
 };
 
 export default Page401;
